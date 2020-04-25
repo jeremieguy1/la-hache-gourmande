fbeaf09d7c10f12acb6b970385cf653d58a0a126
Jérémie <mine.guy09@gmail.com>
2020-04-25 19:33:09 +0200

    [Front] - Clean files

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
-
-/*app.use((req, res, next) => {
-    res.set({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-        "Content-Security-Policy": "default-src 'self'",
-        "X-Content-Security-Policy": "default-src 'self'",
-        "X-WebKit-CSP": "default-src 'self'"    
-    })
-    next();
-});*/
-
 
 app.use(express.static(__dirname + '/dist/la-hache-gourmande')); 
 
