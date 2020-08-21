8b4c0a2222b2f557a8382b5d061bb59f8928580e
Jérémie <mine.guy09@gmail.com>
2020-08-21 16:49:14 +0200

    [Front] - Modify map page

--- src/app/features/map/map.component.ts
+++ src/app/features/map/map.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  PagesNameEnum
+} from './../../shared/enums/pages-name.enum';
+
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+
+  pageName = PagesNameEnum.MAP;
 
   constructor() { }
 
