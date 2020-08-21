3a9e53a117eb92bb1bb6ed7b7ca81234fd19a20b
Jérémie <mine.guy09@gmail.com>
2020-08-21 23:00:56 +0200

    [Front] - Add skiplink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,27 @@ import {
   Component
 } from '@angular/core';
 
+import { Router, NavigationEnd } from '@angular/router';
+
+import {
+  filter
+} from 'rxjs/operators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'la-hache-gourmande';
+  title: string = 'la-hache-gourmande';
+
+  skipLinkPath: string;
+  
+  constructor(private router: Router) {
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      if (!this.router.url.endsWith('#main-content')) {
+          this.skipLinkPath = `${this.router.url}#main-content`;
+      }
+   });
+  }
 }
