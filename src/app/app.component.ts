88af37356b6f96d1fb86a618fd3a009455740791
Jérémie <mine.guy09@gmail.com>
2020-08-22 01:05:19 +0200

    [Front] - Add back-to-top link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { 
-  Component
+  Component, OnInit, HostListener
 } from '@angular/core';
 
 import { Router, NavigationEnd } from '@angular/router';
+
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 import {
   filter
@@ -12,17 +14,40 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'la-hache-gourmande';
 
+  faArrowUp = faArrowUp;
+
   skipLinkPath: string;
+
+  isScrolled: boolean = false;
   
   constructor(private router: Router) {
-    router.events.pipe(
+    
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.isScrolled = true;
+      } 
+     else if (this.isScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.isScrolled = false;
+      }
+  }
+  ngOnInit() {
+
+    this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)).subscribe(() => {
       if (!this.router.url.endsWith('#main-content')) {
           this.skipLinkPath = `${this.router.url}#main-content`;
       }
    });
+  }
+
+  scroll(): void {
+    console.log(window.pageYOffset > 100)
+    this.isScrolled = window.pageYOffset > 100;
   }
 }
