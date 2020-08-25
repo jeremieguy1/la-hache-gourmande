37fc43964a15e47652a7575c36ea06f724efec41
Jérémie <mine.guy09@gmail.com>
2020-08-25 11:03:44 +0200

    [Front] - Make card a component #34

---
+++ src/app/shared/card/card.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  @Input() title: string;
+
+  @Input() icon;
+
+  @Input() animation?: string;
+
+  @Input() color?: string;
+
+  cardClass: string;
+
+  iconClass: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.animation ?
+      this.cardClass = "card animate__animated " + this.animation:
+      this.cardClass = "card";
+
+      this.color ?
+        this.iconClass = "is-size-1 " + this.color:
+        this.iconClass = "is-size-1";
+  }
+
+}
