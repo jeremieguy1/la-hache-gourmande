c32ff67e19d1ada07e88236f23168348276cec85
Jérémie <mine.guy09@gmail.com>
2020-08-29 22:07:44 +0200

    [Front] - Modifies server.js #9

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var router = express.Router();
 
 app.listen(process.env.PORT || 8080);
 
 app.use(express.static(__dirname + '/dist/la-hache-gourmande'));
-app.use(express.static(__dirname + '/dist/la-hache-gourmande/en'));
-app.use(express.static(__dirname + '/dist/la-hache-gourmande/fr'));
 
-app.get('/en/*', function(req, res) {
-  req.baseUrl
-  console.log('Sending index for EN : ' + req.baseUrl);
-  res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/en/index.html'));
-})
 
-app.get('/fr/*', function(req, res) {
-  console.log('Sending index for FR');
-  res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
+app.get('/', function(req, res) {
+  if(req.url.includes("en")) {
+    console.log('Sending en index ' + req.url);
+    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/en/index.html'));
+  } else if (req.url.includes("fr")) {
+    console.log('Sending fr index ' + req.url);
+    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
+  } else {
+    console.log('Sending fr index default' + req.url);
+    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
+  }
 })
 
 app.get('/*', function(req, res) {
-  console.log('Sending index FR by default');
-  res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
+  if(req.url.includes("en")) {
+    console.log('Sending en index /* ' + req.url);
+    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/en/index.html'));
+  } else if (req.url.includes("fr")) {
+    console.log('Sending fr index /* ' + req.url);
+    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
+  } else {
+    console.log('Sending fr index default /* ' + req.url);
+    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
+  }
+})
+
+app.get('*', function(req, res) {
+    console.log('Sending fr index * ' + req.url);
+    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
 })
 
 console.log('Listening on ' + process.env.PORT || 8080)
