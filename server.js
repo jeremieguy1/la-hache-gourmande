695490221b30d9461aefaa7dcb9879da3f997443
Jérémie <mine.guy09@gmail.com>
2020-08-29 17:57:13 +0200

    [Front] - Modifies the express server config #9

--- server.js
+++ server.js
@@ -9,28 +9,20 @@ app.use(express.static(__dirname + '/dist/la-hache-gourmande'));
 app.use(express.static(__dirname + '/dist/la-hache-gourmande/en'));
 app.use(express.static(__dirname + '/dist/la-hache-gourmande/fr'));
 
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
-  });
-  // define the home page route
-  router.get('/en/*', function(req, res) {
-    console.log("EN : " + __dirname)
-    // res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/en/index.html'));
-  });
-  // define the about route
-  router.get('/fr/*', function(req, res) {
-    console.log("FR : "+ __dirname)
-    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
-  });
+app.get('/en/*', function(req, res) {
+  req.baseUrl
+  console.log('Sending index for EN : ' + req.baseUrl);
+  res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/en/index.html'));
+})
 
-  // define the about route
-  router.get('/*', function(req, res) {
-    console.log("FR : "+ __dirname)
-    res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
-  });
+app.get('/fr/*', function(req, res) {
+  console.log('Sending index for FR');
+  res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
+})
 
-    module.exports = router;
-
+app.get('/*', function(req, res) {
+  console.log('Sending index FR by default');
+  res.sendFile(path.join(__dirname + '/dist/la-hache-gourmande/fr/index.html'));
+})
 
 console.log('Listening on ' + process.env.PORT || 8080)
