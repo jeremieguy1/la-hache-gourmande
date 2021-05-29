32118bef37b6dde132d6ac9fa4fe7c12699c9d5d
Jérémie Guy <44225233+jeremieguy1@users.noreply.github.com>
2021-05-29 11:58:00 +0200

    [FRONT] - Add compodoc to project (#55)
    * [FRONT] - Add compodoc to project
    
    * [FRONT] - Add some tsdoc for compodoc

--- src/app/shared/directives/bold-by-name.directive.ts
+++ src/app/shared/directives/bold-by-name.directive.ts
@@ -1,42 +1,37 @@
-import {
-  Directive,
-  ElementRef,
-  AfterViewInit
-} from '@angular/core';
+import { Directive, ElementRef, AfterViewInit } from "@angular/core";
 
-import {
-  BoldNamesEnum
-} from '../../config/enums/bold-names.enum';
+import { BoldNamesEnum } from "../../config/enums/bold-names.enum";
 
 @Directive({
-  selector: '[appBoldByName]'
+	selector: "[appBoldByName]",
 })
 export class BoldByNameDirective implements AfterViewInit {
+	constructor(private el: ElementRef) {}
 
-  constructor(private el: ElementRef) { }
+	ngAfterViewInit() {
+		this.boldContent();
+	}
 
-  ngAfterViewInit() {
-    this.boldContent();
-    
-  }
-
-  /**
-   * Bold a list of words defined in BoldNamesEnum.
-   */
-  boldContent(): void {
-    const words = this.el.nativeElement.childNodes[0].innerHTML.split(' ');
-    for(const [_i, word] of words.entries()) {
-      let currentWord:string[] = word.split('_');
-      if (currentWord.length > 1) {
-        const tmp = currentWord;
-        currentWord = [];
-        currentWord.push(tmp.join(' '));
-      }
-      if (Object.values(BoldNamesEnum).find(e => e === currentWord[0].toLowerCase())) {
-        words[_i] = currentWord[0].bold();
-      }
-    }
-    this.el.nativeElement.childNodes[0].innerHTML = words.join(' ');
-  }
-
+	/**
+	 * Bold a list of words defined in BoldNamesEnum.
+	 */
+	boldContent(): void {
+		const words = this.el.nativeElement.childNodes[0].innerHTML.split(" ");
+		for (const [_i, word] of words.entries()) {
+			let currentWord: string[] = word.split("_");
+			if (currentWord.length > 1) {
+				const tmp = currentWord;
+				currentWord = [];
+				currentWord.push(tmp.join(" "));
+			}
+			if (
+				Object.values(BoldNamesEnum).find(
+					(e) => e === currentWord[0].toLowerCase()
+				)
+			) {
+				words[_i] = currentWord[0].bold();
+			}
+		}
+		this.el.nativeElement.childNodes[0].innerHTML = words.join(" ");
+	}
 }
--- src/app/shared/image-page/image-page.component.ts
+++ src/app/shared/image-page/image-page.component.ts
@@ -1,74 +1,67 @@
-import {
-  Component,
-  OnInit,
-  Input 
-} from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 import {
-  Router, 
-  UrlTree,
-  UrlSegmentGroup,
-  PRIMARY_OUTLET,
-  ActivatedRoute, 
-} from '@angular/router';
+	Router,
+	UrlTree,
+	UrlSegmentGroup,
+	PRIMARY_OUTLET,
+	ActivatedRoute,
+} from "@angular/router";
 
-import {
-  ImagePageContent,
-} from './image-content';
+import { ImagePageContent } from "./image-content";
 
-import {
-  ImagePageContentService
-} from './image-page-content.service';
+import { ImagePageContentService } from "./image-page-content.service";
 
-import {
-  StepsId
-} from '../../config/enums/steps-id.enum';
+import { StepsId } from "../../config/enums/steps-id.enum";
 
-import {
-  PagesNameEnum
-} from '../../config/enums/pages-name.enum';
+import { PagesNameEnum } from "../../config/enums/pages-name.enum";
 
 @Component({
-  selector: 'app-image-page',
-  templateUrl: './image-page.component.html',
-  styleUrls: ['./image-page.component.scss']
+	templateUrl: "./image-page.component.html",
+	styleUrls: ["./image-page.component.scss"],
 })
 export class ImagePageComponent implements OnInit {
+	imagePageContent: ImagePageContent;
 
-  @Input() targetImagePage: string;
+	id: string;
 
-  imagePageContent: ImagePageContent;
+	constructor(
+		private router: Router,
+		private route: ActivatedRoute,
+		private imagePageContentService: ImagePageContentService
+	) {}
 
-  id: string;
+	ngOnInit(): void {
+		this.route.paramMap.subscribe((params) => {
+			if (params.get("id")) {
+				this.id = params.get("id");
+			} else {
+				const tree: UrlTree = this.router.parseUrl(this.router.url);
+				const primary: UrlSegmentGroup =
+					tree.root.children[PRIMARY_OUTLET];
+				Object.values(PagesNameEnum).forEach((element) => {
+					if (element === primary.segments[0].toString()) {
+						this.id =
+							StepsId[
+								this.computeEnumKey(
+									primary.segments[0].toString()
+								)
+							];
+						return;
+					}
+				});
+			}
+			this.imagePageContent = this.imagePageContentService.getContent(
+				this.id
+			);
+		});
+	}
 
-  constructor(private router: Router,
-    private route: ActivatedRoute,
-    private imagePageContentService: ImagePageContentService) { }
-
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      if (params.get('id')) {
-        this.id = params.get('id');
-      } else {
-        const tree: UrlTree = this.router.parseUrl(this.router.url);
-        const primary: UrlSegmentGroup = tree.root.children[PRIMARY_OUTLET];
-        Object.values(PagesNameEnum).forEach(element => {
-          if (element === primary.segments[0].toString()) {
-            this.id = StepsId[this.computeEnumKey(primary.segments[0].toString())];
-            return;
-          }
-        })
-      }
-      this.imagePageContent = this.imagePageContentService.getContent(this.id);
-    });
-  }
-
-  /**
-   * Transform a string to an enum key compatible state
-   * @param toCompute Raw key
-   */
-  computeEnumKey(toCompute: string): string {
-    return toCompute.split("-").join('').toUpperCase();
-  }
-
+	/**
+	 * Transform a string to an enum key compatible state
+	 * @param toCompute Raw key
+	 */
+	computeEnumKey(toCompute: string): string {
+		return toCompute.split("-").join("").toUpperCase();
+	}
 }
