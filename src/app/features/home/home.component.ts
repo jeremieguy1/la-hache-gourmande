37fbd4a5b0cf2a5a83ac6854f4f51dc511b94196
Jérémie <mine.guy09@gmail.com>
2021-05-29 11:49:30 +0200

    [FRONT] - Add some tsdoc for compodoc

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,68 +1,64 @@
-import { 
-  Component,
-  OnInit 
-} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { TitleService } from "./../title.service";
 
 import {
-  TitleService
-} from './../title.service';
+	PagesNameEnum,
+	PagesNameTitleEnum,
+} from "./../../config/enums/pages-name.enum";
 
-import { 
-  PagesNameEnum,
-  PagesNameTitleEnum
-} from './../../config/enums/pages-name.enum';
-
-import {
-  CarouselContentService
-} from '../../shared/carousel/carousel-content.service';
+import { CarouselContentService } from "../../shared/carousel/carousel-content.service";
 
 @Component({
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+	/**
+	 * Prefix for asset folder
+	 */
+	ASSETS_PREFIX = "assets/carousel/home/";
 
-  /**
-   * Prefix for asset folder
-   */
-  ASSETS_PREFIX = 'assets/carousel/home/';
+	/**
+	 * Id for the carousel
+	 */
+	pageName: PagesNameEnum = PagesNameEnum.HOME;
 
-  /**
-   * Id for the carousel
-   */
-  pageName: PagesNameEnum = PagesNameEnum.HOME;
+	/**
+	 * Content of the carousel
+	 */
+	carouselContent: string[];
 
-  /**
-   * Content of the carousel
-   */
-  carouselContent: string[];
+	constructor(
+		private carouselContentService: CarouselContentService,
+		private titleService: TitleService
+	) {}
 
-  constructor(private carouselContentService: CarouselContentService,
-    private titleService: TitleService) { }
+	ngOnInit(): void {
+		this.titleService.setPageTitle(PagesNameTitleEnum.HOME);
+		this.carouselContent = this.computeCarouselContent(
+			this.getCarouselContent()
+		);
+	}
 
-  ngOnInit(): void {
-    this.titleService.setPageTitle(PagesNameTitleEnum.HOME);
-    this.carouselContent = this.computeCarouselContent(this.getCarouselContent());
-  }
+	/**
+	 * Call the service which fetch data from carousel service
+	 * @return The date fetched from the service
+	 */
+	getCarouselContent(): string[] {
+		return this.carouselContentService.getCarouselContent(this.pageName);
+	}
 
-  /**
-   * Call the service which fetch data from carousel service
-   * @return The date fetched from the service
-   */
-  getCarouselContent(): string[] {
-    return this.carouselContentService.getCarouselContent(this.pageName);
-  }
-
-  /**
-   * Compute image names fetched with the prefix
-   * @param carouselContent The data fetched
-   * @return The data fetched with the prefix
-   */
-  computeCarouselContent(carouselContent: string[]): string[] {
-    const _carouselContent: string[] = [];
-    for (const [_i, content] of carouselContent.entries()) {
-      _carouselContent[_i] = this.ASSETS_PREFIX + content;
-    }
-    return _carouselContent;
-  }
+	/**
+	 * Compute image names fetched with the prefix
+	 * @param carouselContent The data fetched
+	 * @return The data fetched with the prefix
+	 */
+	computeCarouselContent(carouselContent: string[]): string[] {
+		const _carouselContent: string[] = [];
+		return carouselContent.map(
+			(content, _i) =>
+				(_carouselContent[_i] = this.ASSETS_PREFIX + content)
+		);
+	}
 }
